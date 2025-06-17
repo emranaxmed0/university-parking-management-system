cba7416ed18ae9cad9a3a6dda2f08dd7dfa8fbee
emran-axmed <emranaxmed@gmail.com>
2025-06-17 18:41:31 +0300

    moving navigation to includes

---
+++ includes/nav.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="css/nav.css">
+<nav class="main-nav">
+    <div class="nav-container">
+        <ul class="nav-links">
+            <li><a href="./">Home</a></li>
+            <li><a href="/check-availability.php">Check Availability</a></li>
+            <li><a href="/admin-logs.php">Admin Logs</a></li>
+        </ul>
+        <ul class="auth-links">
+            <li><a href="/login.php">Log In</a></li>
+            <li><a href="/logout.php">Log Out</a></li>
+            <li><a href="/signup.php" class="signup-btn">Sign Up</a></li>
+        </ul>
+    </div>
+</nav>
