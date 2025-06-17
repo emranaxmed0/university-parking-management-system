9f3f239b9798fe5388f7aeecd4df68e5090fea4d
emran-axmed <emranaxmed@gmail.com>
2025-06-17 19:35:57 +0300

    updating the navigation

--- includes/nav.php
+++ includes/nav.php
@@ -1,15 +1,27 @@
 <link rel="stylesheet" href="css/nav.css">
-<nav class="main-nav">
-    <div class="nav-container">
-        <ul class="nav-links">
-            <li><a href="./">Home</a></li>
-            <li><a href="/check-availability.php">Check Availability</a></li>
-            <li><a href="/admin-logs.php">Admin Logs</a></li>
-        </ul>
-        <ul class="auth-links">
-            <li><a href="/login.php">Log In</a></li>
-            <li><a href="/logout.php">Log Out</a></li>
-            <li><a href="/signup.php" class="signup-btn">Sign Up</a></li>
-        </ul>
+<nav>
+    <div class="topnav">
+        <a href="./">Home</a>
+        <a href="/check-availability.php">Check Availability</a>
+        <a href="/admin-logs.php">Admin Logs</a>
+    </div>    
+    <div class="topnav-right">
+            <div class="dropdown">
+                <a href="#">Log In</a>
+                <div class="dropdown-content">
+                    <a href="/login/student_login.php">Student Login</a>
+                    <a href="/login/staff_login.php">Staff Login</a>
+                    <a href="/login/visitor_login.php">Visitor Login</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <a href="#" class="signup-btn">Sign Up</a>
+                <div class="dropdown-content">
+                    <a href="/signup/student_signup.php">Student Sign Up</a>
+                    <a href="/signup/staff_signup.php">Staff Sign Up</a>
+                    <a href="/signup/visitor_signup.php">Visitor Sign Up</a>
+                </div>
+            </div>
+        </div>
     </div>
 </nav>
